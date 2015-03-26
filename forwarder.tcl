ec5a6c7e92b80226d65664f009695a27c6dbc8dd
Emmanuel Frécon <emmanuel@sics.se>
2015-03-26 23:15:11 +0100

    Properly follow containers through their lifecycle, the watch will perdure restarts

--- forwarder.tcl
+++ forwarder.tcl
@@ -18,9 +18,10 @@ set prg_args {
     -method    POST        "HTTP method to use"
     -type      "application/octet-stream" "MIME type for query"
     -keepalive 1           "Keepalive connections"
-    -mapper    {}          "Mapping from component names/ids to topics"
+    -mapper    {}          "Mapping from container names/ids to topics"
     -dryrun    0           "Output what would be done instead"
-    -output    stdout      "Which output of the docker components to listen to"
+    -output    stdout      "Which output of the docker containers to listen to"
+    -retry     5000        "Milliseconds between container attachment retries"
 }
 
 
@@ -95,23 +96,29 @@ if { [llength $argv] > 0 } {
 
 # Callback once HTTP operation has ended, output some logging details
 # depending on the success of the HTTP operation
-proc ::result { component url line tok } {
+proc ::result { container url line tok } {
     global FWD
 
     set ncode [::http::ncode $tok]
     if { $ncode >= 200 && $ncode < 300 } {
-	::docker::log DEBUG "Success for $FWD(-method) HTTP op on $url"
+	docker log DEBUG "Success for $FWD(-method) HTTP op on $url"
     } else {
-	::docker::log WARN "Could not trigger $url: HTTP errcode: $ncode"
+	docker log WARN "Could not trigger $url: HTTP errcode: $ncode"
     }
     ::http::cleanup $tok
 }
 
-# Callback whenever an output line from a docker component has been
+# Callback whenever an output line from a docker container has been
 # captured, generate the appropriate HTTP operation.
-proc ::forward { component url type line } {
+proc ::forward { container url type line } {
     global FWD
 
+    if { $type eq "error" } {
+	docker log WARN "Connection to $container lost,\
+                            retrying in $FWD(-retry) ms"
+	after $FWD(-retry) [list ::init $container $url]
+	return
+    }
     if { $line ne "" } {
 	set URLmatcher {(?x)        # this is _expanded_ syntax
                     ^
@@ -136,54 +143,91 @@ proc ::forward { component url type line } {
 	if { [catch {regexp -- $URLmatcher $url -> \
 			 proto user host port srvurl} res] == 0 } {
 	    if { $user ne "" } {
-		set headers [list Authorization "Basic [binary encode base64 $user]"]
+		set headers [list Authorization \
+				 "Basic [binary encode base64 $user]"]
 	    } else {
 		set headers {}
 	    }
 	    # dryrun will ensure we can test without triggering the
 	    # remote URLs
 	    if { [string is true -strict $FWD(-dryrun)] } {
-		::docker::log NOTICE "Would $FWD(-method) $line to $url\
+		docker log NOTICE "Would $FWD(-method) $line to $url\
                                       (auth: $user)"
 	    } else {
 		# Construct a URL getting command and execute it.
 		# Arrange to schedule a callback to be called once
 		# done, so we can continue with all further lines from
-		# the docker components.
+		# the docker containers.
 		if { [catch {::http::geturl $url \
 				 -headers $headers \
 				 -method $FWD(-method) \
 				 -type $FWD(-type) \
 				 -query $line \
 				 -keepalive $FWD(-keepalive) \
-				 -command [list ::result $component $url $line]} tok] == 0 } {
-		    ::docker::log DEBUG "Successfully posting line to $url"
+				 -command [list ::result \
+					       $container $url $line]} \
+			  tok] == 0 } {
+		    docker log DEBUG "Successfully posting line to $url"
 		} else {
-		    ::docker::log WARN "Cannot post to $url: $tok"
+		    docker log WARN "Cannot post to $url: $tok"
 		}
 	    }
 	} else {
-	    ::docker::log WARN "Cannot understand URL $url: $res"
+	    docker log WARN "Cannot understand URL $url: $res"
 	}
     }
 }
 
 
+proc ::attach { container url } {
+    global DOCKER
+    global FWD
 
-::docker::verbosity $FWD(-verbose)
+    if { [catch {$DOCKER($container) inspect $container} descr] } {
+	if { $FWD(-retry) >= 0 } {
+	    docker log DEBUG "No container, retrying in $FWD(-retry) ms"
+	    after $FWD(-retry) [list ::attach $container $url]
+	}
+    } elseif { [dict exists $descr State Running] \
+		   && [string is true [dict get $descr State Running]] } {
+	docker log NOTICE "Attaching to container $container\
+                           on $FWD(-output)"
+	$DOCKER($container) attach $container \
+	    [list ::forward $container $url] \
+	    stream 1 $FWD(-output) 1
+    } elseif { $FWD(-retry) >= 0 } {
+	docker log DEBUG "No container, retrying in $FWD(-retry) ms"
+	after $FWD(-retry) [list ::attach $container $url]
+    }
+}
+
+
+proc ::init { container url } {
+    global DOCKER
+    global FWD
+
+    # Disconnect if we already have a connection
+    if { [info exists DOCKER($container)] } {
+	$DOCKER($container) disconnect
+	unset DOCKER($container)
+    }
+
+    set DOCKER($container) [docker connect $FWD(-docker)]
+    ::attach $container $url
+    return $DOCKER($container)
+}
+
+
+docker verbosity $FWD(-verbose)
 if { [catch {package require tls} err] == 0 } {
     ::http::register https 443 [list ::tls::socket -tls1 1]
 } else {
-    ::docker::log WARN "Will not be able to handle HTTPS connections!"
+    docker log WARN "Will not be able to handle HTTPS connections!"
 }
 
-# Connect to docker
-set FWD(docker) [docker connect $FWD(-docker)]
-
-# Enumerate components
-foreach { component url } $FWD(-mapper) {
-    $FWD(docker) attach $component [list ::forward $component $url] \
-	stream 1 $FWD(-output) 1
+# Enumerate containers
+foreach { container url } $FWD(-mapper) {
+    ::init $container $url
 }
 
 vwait forever
